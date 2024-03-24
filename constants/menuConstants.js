6a5826e664590d454d9dc071c58c9f634913fc65
nimisha-gj <156162253+nimisha-gj@users.noreply.github.com>
2024-03-24 10:06:14 +0000

    [Nimisha | Rohit] Update menu handler to reply with help message in default case

---
+++ constants/menuConstants.js
@@ -0,0 +1,1 @@
+exports.HELP_MESSAGE = "Type !help to see this message again.";
--- handlers/menuHandler.js
+++ handlers/menuHandler.js
@@ -1,9 +1,18 @@
-exports.menu = (message) =>{
+const { HELP_MESSAGE } = require('../constants/menuConstants');
+
+exports.menu = (message) => {
     if (message.author.bot || !message.channel.type) return;
     if (message.guild) {
       const isGeneralChannelOrThread =
         message.channel.isThread() || message.channel.name === "general";
       if (isGeneralChannelOrThread) return;
     }
-    message.reply(`hey ${message.author.username}`)
+
+    const content = message.content;
+    console.log("reacher here")
+    switch (true) {
+        default:
+            message.reply(HELP_MESSAGE);
+            break;
+    }
 }
