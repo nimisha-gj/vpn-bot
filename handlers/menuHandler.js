56c48a308308330fd03a7b839ff0dd36915b32dc
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-01 21:13:43 +0530

    refactor: display different help message for ubuntu users

--- handlers/menuHandler.js
+++ handlers/menuHandler.js
@@ -1,26 +1,38 @@
-const { HELP_MESSAGE,DISPLAY_HELP } = require('../constants/menuConstants');
-const { addPublicKey } = require('../services/addKey');
+const {
+  HELP_MESSAGE,
+  DISPLAY_HELP,
+  DISPLAY_HELP_UBUNTU,
+} = require("../constants/menuConstants");
+const { addPublicKey } = require("../services/addKey");
+const { removePublicKey } = require("../services/removeKeys");
 
-exports.menu = async(message) => {
-    if (message.author.bot || !message.channel.type) return;
-    if (message.guild) {
-      const isGeneralChannelOrThread =
-        message.channel.isThread() || message.channel.name === "general";
-      if (isGeneralChannelOrThread) return;
-    }
+exports.menu = async (message) => {
+  if (message.author.bot || !message.channel.type) return;
+  if (message.guild) {
+    const isGeneralChannelOrThread =
+      message.channel.isThread() || message.channel.name === "general";
+    if (isGeneralChannelOrThread) return;
+  }
 
-    const content = message.content;
-    switch (true) {
-      case content === "!help":
-          message.reply(DISPLAY_HELP);
-          break;
-      
-      case content.startsWith("/add"):
-         addPublicKey(message)
-         break;
+  const content = message.content;
+  switch (true) {
+    case content === "/help":
+      message.reply(DISPLAY_HELP);
+      break;
+    case content === "/help-ubuntu":
+      message.reply(DISPLAY_HELP_UBUNTU);
+      break;
 
-        default:
-            message.reply(HELP_MESSAGE);
-            break;
-    }
-}
+    case content.startsWith("/add"):
+      addPublicKey(message);
+      break;
+
+    case content.startsWith("/remove"):
+      removePublicKey(message);
+      break;
+
+    default:
+      message.reply(HELP_MESSAGE);
+      break;
+  }
+};
